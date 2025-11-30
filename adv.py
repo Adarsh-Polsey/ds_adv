7008b97c6872e4cbffcee2610458a0d6e1236cf7
Adarsh Polsey <69569984+Adarsh-Polsey@users.noreply.github.com>
2025-11-30 12:59:55 +0530

    Refactor sales prediction function in adv.py

--- adv.py
+++ adv.py
@@ -6,17 +6,17 @@ import streamlit as st
 import os
 
 print(os.path.dirname(os.path.abspath(__file__)))
+def func():
+    model=joblib.load(open("linear_regression_model.joblib"))
+    st.title("Sales prediction app")
 
-# model=joblib.load(open("ds_adv/linear_regression_model.joblib"))
-# st.title("Sales prediction app")
-
-# TV=st.number_input("TV adv budget",min_value=0.0)
-# Radio=st.number_input("Radio adv budget",min_value=0.0)
-# Newspaper=st.number_input("Newspaper adv budget",min_value=0.0)
+    TV=st.number_input("TV adv budget",min_value=0.0)
+    Radio=st.number_input("Radio adv budget",min_value=0.0)
+    Newspaper=st.number_input("Newspaper adv budget",min_value=0.0)
 
 
-# if st.button('predict sales'):
-#     input=np_array[[TV,Radio,Newspaper]]
-#     prediction=model.predict(input_data)[0]
-    # st.success(f"predict sales:{prediction:.2f}")
+    if st.button('predict sales'):
+        input=np_array[[TV,Radio,Newspaper]]
+        prediction=model.predict(input_data)[0]
+        st.success(f"predict sales:{prediction:.2f}")
     
