d2805650c55b7ed0548c06b3afea780daffbc981
Adarsh Polsey <69569984+Adarsh-Polsey@users.noreply.github.com>
2025-11-30 13:10:06 +0530

    Update adv.py for streamlib integration and input logic
    Refactor sales prediction app to use streamlib and reorganize input handling.

--- adv.py
+++ adv.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import numpy as np
 import joblib
-import streamlit as st
+import streamlib as st
 
-model=joblib.load(open("linear_regression_model.joblib"))
-st.title("Sales prediction app")
-
-TV=st.number_input("TV adv budget",min_value=0.0)
-Radio=st.number_input("Radio adv budget",min_value=0.0)
-Newspaper=st.number_input("Newspaper adv budget",min_value=0.0)
+import os
 
 
-if st.button('predict sales'):
-    input=np_array[[TV,Radio,Newspaper]]
-    prediction=model.predict(input_data)[0]
-    st.success(f"predict sales:{prediction:.2f}")
+print(os.path.dirname(os.path.abspath(__file__)))
+def func():
+    model=joblib.load(open("linear_regression_model.joblib"))
+    st.title("Sales prediction app")
+
+    TV=st.number_input("TV adv budget",min_value=0.0)
+    Radio=st.number_input("Radio adv budget",min_value=0.0)
+    Newspaper=st.number_input("Newspaper adv budget",min_value=0.0)
+
+
+    if st.button('predict sales'):
+        input=np_array[[TV,Radio,Newspaper]]
+        prediction=model.predict(input_data)[0]
+        st.success(f"predict sales:{prediction:.2f}")
     
