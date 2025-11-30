da7c73cc5961c7c587b496826b6f012619e6d4f4
Adarsh Polsey <69569984+Adarsh-Polsey@users.noreply.github.com>
2025-11-30 13:04:41 +0530

    Refactor sales prediction app code structure

--- adv.py
+++ adv.py
@@ -3,20 +3,16 @@ import numpy as np
 import joblib
 import streamlib as st
 
-import os
+model=joblib.load(open("linear_regression_model.joblib"))
+st.title("Sales prediction app")
 
-print(os.path.dirname(os.path.abspath(__file__)))
-def func():
-    model=joblib.load(open("linear_regression_model.joblib"))
-    st.title("Sales prediction app")
-
-    TV=st.number_input("TV adv budget",min_value=0.0)
-    Radio=st.number_input("Radio adv budget",min_value=0.0)
-    Newspaper=st.number_input("Newspaper adv budget",min_value=0.0)
+TV=st.number_input("TV adv budget",min_value=0.0)
+Radio=st.number_input("Radio adv budget",min_value=0.0)
+Newspaper=st.number_input("Newspaper adv budget",min_value=0.0)
 
 
-    if st.button('predict sales'):
-        input=np_array[[TV,Radio,Newspaper]]
-        prediction=model.predict(input_data)[0]
-        st.success(f"predict sales:{prediction:.2f}")
+if st.button('predict sales'):
+    input=np_array[[TV,Radio,Newspaper]]
+    prediction=model.predict(input_data)[0]
+    st.success(f"predict sales:{prediction:.2f}")
     
